2f0cd4f8085b6f9ddd1211ff14edadbb82784df1
banga <dennisbanga@gmail.com>
2015-05-19 11:09:20 +0300

    added a controller method on HelloWorldPageController class

--- omod/src/main/java/org/openmrs/module/patientmodule/page/controller/HelloWorldPageController.java
+++ omod/src/main/java/org/openmrs/module/patientmodule/page/controller/HelloWorldPageController.java
@@ -1,8 +1,17 @@
 package org.openmrs.module.patientmodule.page.controller;
 
+
+import org.openmrs.api.context.Context;
+import org.openmrs.ui.framework.page.PageModel;
+
 /**
  * Created by banga on 5/14/15.
  */
 
 public class HelloWorldPageController {
+
+    public void controller(PageModel model) {
+        model.addAttribute("authenticatedUser", Context.getAuthenticatedUser());
+    }
+
 }
